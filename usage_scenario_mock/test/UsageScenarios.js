3eed5c0f58eb5d91ba745fb4ec3ee6e7958131f0
Niklas Ullenboom <niklas.ullenboom@paluno.uni-due.de>
2019-11-05 15:44:15 +0100

    Deletes spaces in test

--- usage_scenario_mock/test/UsageScenarios.js
+++ usage_scenario_mock/test/UsageScenarios.js
@@ -2,14 +2,24 @@ const AvgOnDemand = artifacts.require("./AvgOnDemand.sol");
 const AvgOnDemandTwo = artifacts.require("./AvgOnDemandTwo.sol");
 
 contract("First UsageScenario", async accounts => {
-  it("Implement variant one ", async () => {
+  it("Implement variant one", async () => {
     const avgOnDemand = await AvgOnDemand.new();
     const addTransaction = await avgOnDemand.add(10);
     const addTransaction2 = await avgOnDemand.add(10);
     const avgTransaction = await avgOnDemand.calc();
     const avgTransaction2 = await avgOnDemand.calc();
   });
-  it("Implement variant two ", async () => {
+  it("Implement variant two", async () => {
+    const avgOnDemand = await AvgOnDemandTwo.new();
+    const addTransaction = await avgOnDemand.add(10);
+    const addTransaction2 = await avgOnDemand.add(10);
+    const avgTransaction = await avgOnDemand.calc();
+    const avgTransaction2 = await avgOnDemand.calc();
+  });
+});
+
+contract("First UsageScenario", async accounts => {
+  it("Implement variant two2", async () => {
     const avgOnDemand = await AvgOnDemandTwo.new();
     const addTransaction = await avgOnDemand.add(10);
     const addTransaction2 = await avgOnDemand.add(10);
@@ -19,13 +29,13 @@ contract("First UsageScenario", async accounts => {
 });
 
 contract("Second UsageScenario", async accounts => {
-  it("Implement variant one ", async () => {
+  it("Implement variant one", async () => {
     const avgOnDemand = await AvgOnDemand.new();
     const addTransaction = await avgOnDemand.add(10);
     const avgTransaction = await avgOnDemand.calc();
   });
 
-  it("Implement variant two ", async () => {
+  it("Implement variant two", async () => {
     const avgOnDemand = await AvgOnDemandTwo.new();
     const addTransaction = await avgOnDemand.add(10);
     const avgTransaction = await avgOnDemand.calc();
